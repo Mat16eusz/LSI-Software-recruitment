f17924547e7d10dd22df9f8b34c4190cdcf45aab
Mateusz Jasiak <mateusz.jasiak.dev@gmail.com>
2022-07-04 12:52:28 +0200

    Saving data from API to the database

--- app/src/main/java/com/example/lsisoftware/MainActivity.java
+++ app/src/main/java/com/example/lsisoftware/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.lsisoftware;
 
 import static com.example.lsisoftware.APIClient.getClient;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.example.lsisoftware.database.AppDatabase;
+import com.example.lsisoftware.database.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                     UsersDataAPIOne usersDataAPISOne = response.body();
 
                     for (int i = 0; i < usersDataAPISOne.getLimit(); i++) {
-
+                        saveUser(usersDataAPISOne.getId(i), usersDataAPISOne.getScreenname(i), APIService.BASE_ONE_URL + "users");
                     }
                 }
             }
@@ -66,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                     List<UsersDataAPITwo> usersDataAPITwo = response.body();
 
                     for (UsersDataAPITwo userDataAPITwo : usersDataAPITwo) {
-
+                        saveUser(userDataAPITwo.getLogin(), userDataAPITwo.getAvatar_url(), APIService.BASE_TWO_URL + "users");
                     }
                 }
             }
@@ -76,5 +81,33 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("Retrofit", "Error: ", t);
             }
         });
+    }
+
+    private void saveUser(String name, String avatar, String sourceAPI) {
+        AppDatabase appDatabase = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        User user = new User();
+        user.name = name;
+        user.avatar = avatar;
+        user.sourceAPI = sourceAPI;
+
+        appDatabase.userDAO().insertUser();
+
+        finish();
+    }
+
+    private void loadUserList() {
+        AppDatabase appDatabase = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        List<User> userList = appDatabase.userDAO().getAllUsers();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 100) {
+            loadUserList();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
